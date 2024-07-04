df7c0265002e6508c2045ba240ae2b5d0ce505a0
jgtaeeun <jas49258667@gmail.com>
2024-07-04 11:22:41 +0900

    0704 스프링부터 시작하기

---
+++ Chapter01/src/main/java/com/rubypaper/domain/BoardVO.java
@@ -0,0 +1,14 @@
+package com.rubypaper.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class BoardVO {
+
+    private int seq;
+
+}
